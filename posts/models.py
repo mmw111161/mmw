f3073ac587632ada92015f6c72e9c2e2cf7bbe7a
melvynmwright <melwright61@virginmedia.com>
2021-12-21 16:28:09 +0000

    Added new posts app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+
+class PostsIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
 from django.db import models
 
 from wagtail.core.models import Page
