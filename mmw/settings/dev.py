66162b6fb9a3ebda76f3f44d1bb69fda8c0ad0c4
melvynmwright <melwright61@virginmedia.com>
2021-12-21 14:52:46 +0000

    Intial commit

---
+++ mmw/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-i%#0c33gakxr$*&izm9h6@he!^&*#h-0go53c+0cvrervo5#by'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
